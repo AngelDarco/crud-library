4ace95a3a809c4d35b2d0342e790ae28c38477e1
angeldarco <angel92-eme@hotmail.com>
2022-05-23 18:40:22 +0000

    context updated

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,18 +1,39 @@
-import { createContext, useContext} from "react";
+import { useEffect, useState, createContext, useContext} from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../fbConfig/firebase-config";
 
 const context = createContext();
 const userValue = {
-    user: 'Darco',
-    pass: '12345'
+    uid: '',
+    user: '',
 }
 
 export const Auth = ()=>{
     return useContext(context)
 }
 
-export const AuthProvider = ({children})=>{
+export const AuthProvider = ({children })=>{
+    const [ userData, setUserData ] = useState(userValue);
+
+    useEffect(()=>{
+        (async ()=> {
+            await onAuthStateChanged(auth, currentUser =>{
+                currentUser ?
+                setUserData({
+                    uid: currentUser.uid,
+                    user: currentUser.email
+                })
+                :
+                setUserData({
+                    uid: '',
+                    user: ''
+                })
+            })
+        })()
+    },[])
+
     return(
-        <context.Provider value={userValue}>
+        <context.Provider value={ { userData, setUserData } }>
             {children}
         </context.Provider>
     )
