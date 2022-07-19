e1047a9bf2ca23f605e86c4e22642dd0bee9ee4d
angeldarco <angel92-eme@hotmail.com>
2022-07-19 17:44:23 +0000

    card buttons add

--- src/components/card/Cards.jsx
+++ src/components/card/Cards.jsx
@@ -1,40 +1,47 @@
-import './Cards.scss';
-import { collection, getDocs } from "firebase/firestore";
-import { useEffect, useState} from 'react';
-import { db } from '../../fbConfig/firebase-config';
-import Loading from '../assets/loading/Loading';
+import "./Cards.scss";
+import Loading from "../assets/loading/Loading";
+import useData from "../hooks/useData";
+import { FaDownload } from "react-icons/fa";
+import { BsFillBookmarkStarFill, BsBookmarkXFill } from "react-icons/bs";
+import { AiOutlineLike, AiTwotoneLike } from "react-icons/ai";
+// import { useState, useRef, useEffect } from "react";
+//import { auth } from '../../fbConfig/firebase-config';
 
-const Cards = ()=>{
-const [data, setData ] = useState('');
-const [loading, setLoading ] = useState(true);
+const Cards = () => {
+  const { data, loading } = useData();
 
-useEffect(()=>{
-  data === "" &&
-  (async ()=>{
-    const img = []; 
-    const arr = await getDocs(collection(db,'products'));
-    arr.forEach(doc => img.push(doc.data()))
-    setData(...data,img)
-    setLoading(false)
-  })()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-},[])
+  const handlerLikes = (res) => {
+    console.log(res)
+  };
 
-    return(
-      <div className="cardsContainer" onLoad={fetch}>
-          {
-            loading ?
-            <Loading/>
-            :
-            data.map(res => 
-              <div className="card" key={res.name}>
-                <img src={res.url} alt={res.name} />
-                <p>{res.name}</p>
-                <p>{res.autor}</p>
+  return (
+    <div className="cardsContainer" onLoad={fetch}>
+      {loading ? (
+        <Loading />
+      ) : (
+        data?.map((res) => (
+          <div className="card" key={res.id}>
+            <div className="icons">
+              <div>
+                { res.avalible === 'true' ? <BsFillBookmarkStarFill className="avalible"/> 
+                                          : <BsBookmarkXFill className="unavalible"/>}
               </div>
-               )
-              } 
-        </div> 
-    )
-}
+              <div>
+                <FaDownload className={res.download === 'true' ? 'download' : 'undownload'}/>
+
+                <div className="likes" onClick={()=>handlerLikes(res)}>
+                  { res.like === 'true' ? <AiTwotoneLike className="like" /> 
+                                        : <AiOutlineLike className="dislike"/>}
+                </div>
+              </div>
+            </div>
+            <img src={res.url} alt={res.name} />
+            <p>{res.name}</p>
+            <p>{res.autor}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
 export default Cards;
