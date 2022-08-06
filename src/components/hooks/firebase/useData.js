e64db9aed987728b3645af8451b826ca309eafc7
angeldarco <angel92-eme@hotmail.com>
2022-08-06 07:31:51 +0000

    add hooks to access realtime db firebase

--- src/components/hooks/firebase/useData.js
+++ src/components/hooks/firebase/useData.js
@@ -1,41 +1,70 @@
 import { getDatabase, ref, set, onValue, update } from 'firebase/database';
+import { getStorage, uploadBytes, ref as storageRef, getDownloadURL} from 'firebase/storage';
 import { Auth } from '../../../context/Context';
 import uuid from 'react-uuid';
 
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const useData = ()=>{
+const [ loading, setLoading ] = useState(true);
 
 const db = getDatabase();
 const {userData} = Auth();
 const { uid } = userData;
 
 function WriteData({autor,name,img}){
-    const key = uuid()
-    set(ref(db,'books/'+uid+'/'+key),{
-        id: key,
-        autor,
-        name,
-        img,
-        avalible: true,
-        download: false,
-        like: false
-    })
-} 
+    if(!autor || !name || !img) return
+    const loadImg = ()=>{
+        const storage = getStorage();
+        const refImg =  storageRef(storage,'Books/'+uuid()+img.name);
+                  
+        return new Promise((resolve,reject) =>
+                    uploadBytes(refImg,img)
+                    .then(snapshot => {
+                        getDownloadURL(storageRef(storage, snapshot.metadata.fullPath))
+                        .then( link => {
+                            resolve(link)
+                        });
+                    })
+                    .catch(err => reject(err.message))
+                )
+            }
+        
+        return new Promise( (resolve,reject) =>
+        loadImg()
+            .then(res => {
+                     set(ref(db,'books/'+uid+'/'+uuid()),{
+                        id: uuid(),
+                        autor,
+                        name,
+                        img:res,
+                        avalible: true,
+                        download: false,
+                        like: false
+                    })
+                    .then(()=>{
+                        resolve()
+                    })               
+                })
+                .catch( err => reject(err.message))
+            )
+}
 
 function ReadData(id='public'){
     const [ data, setData ] = useState([]);
-    useEffect(()=>{
-            onValue(ref(db, 'books/' + id), snapshot => {
-                setData([]);
-                const database = snapshot.val();
-                if (database !== null) {
-                    Object.values(database).map(db => {
-                        setData(oldData => [...oldData, db]);
-                    });
-                }
-            })
-    },[id]);
+            useEffect(()=>{
+                onValue(ref(db, 'books/' + id), snapshot => {
+                    setData([]);
+                    const database = snapshot.val();
+                    if (database !== null) {
+                        Object.values(database).map(db => {
+                            setData(oldData => [...oldData, db]);
+                        });
+                        setLoading(false);
+                    }
+                })
+                return ()=> setLoading(true)
+            },[id])
     return data;
 }
 
@@ -49,6 +78,6 @@ function UpdateData({id,like,avalible,download}){
         update(ref(db,'books/'+uid+'/'+id),newData)
 }
 
-    return { WriteData, ReadData, UpdateData };
+    return { WriteData, ReadData, UpdateData, loading };
 }
 export default useData;
