8d97ee1b645aa2b541087ad6f0e89a2332b30102
angeldarco <angel92-eme@hotmail.com>
2022-08-12 18:58:00 +0000

    add books to firebase db

---
+++ src/components/hooks/firebase/UseDownload.js
@@ -0,0 +1,22 @@
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import { useEffect, useState } from 'react';
+
+const useDownload = ()=>{
+    const storage = getStorage();
+    const fileRef = ref(storage,'Pdf/SkaterBoy.docx');
+    const [ url, setUrl ] = useState('');
+
+    useEffect(()=>{
+           getDownloadURL(ref(storage,fileRef))
+        .then( url => {
+            setUrl(url)
+        })
+        .catch( err => {
+            console.log(err)
+        })
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+    return url;                          
+}
+export default useDownload;
