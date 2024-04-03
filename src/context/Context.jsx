ecde4db508313a3e22e66e60f816ebb0a8cddd9e
angeldarco <angel92-eme@hotmail.com>
2024-04-03 09:43:25 -0500

    :racehorse: firebase config updated

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,41 +1,40 @@
-import { useEffect, useState, createContext, useContext} from "react";
+import { useEffect, useState, createContext, useContext } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../fbConfig/firebase-config";
+import { auth } from "../firebase/config";
 
 const context = createContext();
 const userValue = {
-    uid: '',
-    user: '',
-}
+  uid: "",
+  user: "",
+};
 
-export const Auth = ()=>{
-    return useContext(context)
-}
+export const Auth = () => {
+  return useContext(context);
+};
 
-export const AuthProvider = ({children })=>{
-    const [ userData, setUserData ] = useState(userValue);
+export const AuthProvider = ({ children }) => {
+  const [userData, setUserData] = useState(userValue);
 
-    useEffect(()=>{
-        (async ()=> {
-            await onAuthStateChanged(auth, currentUser =>{
-                currentUser ?
-                setUserData({
-                    uid: currentUser.uid,
-                    user: currentUser.email,
-                })
-                :
-                setUserData({
-                    uid: '',
-                    user: ''
-                })
+  useEffect(() => {
+    (async () => {
+      await onAuthStateChanged(auth, (currentUser) => {
+        currentUser
+          ? setUserData({
+              uid: currentUser.uid,
+              user: currentUser.email,
             })
-        })()
-        return ()=> setUserData(userValue)
-    },[])
+          : setUserData({
+              uid: "",
+              user: "",
+            });
+      });
+    })();
+    return () => setUserData(userValue);
+  }, []);
 
-    return(
-        <context.Provider value={ { userData, setUserData } }>
-            {children}
-        </context.Provider>
-    )
-}
+  return (
+    <context.Provider value={{ userData, setUserData }}>
+      {children}
+    </context.Provider>
+  );
+};
